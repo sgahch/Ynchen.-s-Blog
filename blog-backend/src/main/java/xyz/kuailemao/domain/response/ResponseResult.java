c95c70ca367faa7b4757868e118460ae69cbd40e
Ynchen. <hq1911779729@outlook.com>
2025-09-04 09:19:20 +0800

    硬编码调用了 fastjson

--- blog-backend/src/main/java/xyz/kuailemao/domain/response/ResponseResult.java
+++ blog-backend/src/main/java/xyz/kuailemao/domain/response/ResponseResult.java
@@ -1,8 +1,12 @@
 package xyz.kuailemao.domain.response;
 
-import com.alibaba.fastjson2.JSONObject;
-import com.alibaba.fastjson2.JSONWriter;
+// 1. 删掉所有 fastjson 的 import
+// import com.alibaba.fastjson2.JSONObject;
+// import com.alibaba.fastjson2.JSONWriter;
+
+// 2. 引入 Jackson 的 ObjectMapper
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +19,7 @@ import xyz.kuailemao.enums.RespEnum;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+// 这个 Jackson 注解可以确保在序列化时，值为null的字段不会被包含在JSON结果中
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseResult<T> {
     /**
@@ -54,7 +59,6 @@ public class ResponseResult<T> {
         return new ResponseResult<>(RespEnum.SUCCESS.getCode(), msg, data);
     }
 
-
     /**
      * 失败响应，不需要返回数据
      */
@@ -68,22 +72,32 @@ public class ResponseResult<T> {
     public static <T> ResponseResult<T> failure() {
         return new ResponseResult<>(RespEnum.FAILURE.getCode(), RespEnum.FAILURE.getMsg(), null);
     }
+
     public static <T> ResponseResult<T> failure(String msg) {
         return new ResponseResult<>(RespEnum.FAILURE.getCode(), msg, null);
     }
-    public static <T> ResponseResult<T> failure(T data)  {
+
+    public static <T> ResponseResult<T> failure(T data) {
         return new ResponseResult<>(RespEnum.FAILURE.getCode(), RespEnum.FAILURE.getMsg(), data);
     }
 
 
+    // 3. 改造 asJsonString 方法，使用 Jackson
     /**
      * 转json字符串
      *
      * @return {@link String}
      */
     public String asJsonString() {
-        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+        try {
+            // ObjectMapper 是 Jackson 库的核心，用于序列化和反序列化
+            ObjectMapper objectMapper = new ObjectMapper();
+            // writeValueAsString 方法可以将任何Java对象转换为JSON字符串
+            return objectMapper.writeValueAsString(this);
+        } catch (Exception e) {
+            // 异常处理，防止在序列化失败时整个应用崩溃
+            // 生产环境中建议使用日志记录 e.printStackTrace();
+            return "{\"code\":500,\"msg\":\"Error serializing response object\"}";
+        }
     }
-
-
 }
